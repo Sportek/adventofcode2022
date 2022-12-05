c8100f02aa53ed3af30d02ad05b0f3c0bfe13d5d
Sportek <gablandry31@gmail.com>
2022-12-04 20:20:24 -0500

    adding some informations pages

--- 4dec/second.py
+++ 4dec/second.py
@@ -1,14 +1,14 @@
-def camp_cleanup_1(path):
+def camp_cleanup_2(path):
     with open(path) as file:
         count = 0
         for line in file:
             line.replace("\n", "")
             first = [i for i in range(int(line.split(",")[0].split("-")[0]), int(line.split(",")[0].split("-")[1]) + 1)]
             second = [i for i in range(int(line.split(",")[1].split("-")[0]), int(line.split(",")[1].split("-")[1]) + 1)]
-            if all(item in first for item in second) or all(item in second for item in first):
+            if any(item in first for item in second) or any(item in second for item in first):
                 count += 1
     return count
 
 
 if __name__ == '__main__':
-    print(camp_cleanup_1(path="input.txt"))
+    print(camp_cleanup_2(path="input.txt"))
