7efe322f0709264ef696fd1567ddb8df7f7009bc
Sportek <gablandry31@gmail.com>
2022-12-05 21:39:42 -0500

    Day 5

---
+++ 5dec/second.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+def supplystakcs(path):
+    important_lines = [1, 5, 9, 13, 17, 21, 25, 29, 33]
+    matrice = []
+    actions = []
+    with open(path) as file:
+        for line in file:
+            line.replace("\n", "")
+            if line.startswith(" 1"):
+                continue
+            elif line.startswith("move"):
+                numbers = (int(line.split(" ")[1]), int(line.split(" ")[3]) - 1, int(line.split(" ")[5]) - 1)
+                actions.append(numbers)
+            elif line == "\n":
+                continue
+            else:
+                matrice_line = []
+                for number in important_lines:
+                    check = ''
+                    if len(line) - 1 >= number:
+                        check = line[number] if line[number] != " " else ''
+                    matrice_line.append(check)
+                matrice.append(matrice_line)
+    matrice = np.array(matrice).transpose().tolist()
+    matrice = list(filter(None, [list(filter(None, m)) for m in matrice]))
+
+    for action in actions:
+        to_add = matrice[action[1]][0:action[0]]
+        for i in reversed(to_add):
+            matrice[action[2]].insert(0, i)
+        for _ in range(action[0]):
+            del matrice[action[1]][0]
+    print(matrice)
+    return ''.join([element[0] for element in matrice])
+
+
+if __name__ == '__main__':
+    print(supplystakcs("code.txt"))
